153d19096eec3e80bdf654501fc7fca4d42e6ace
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-12 22:06:14 -0300

    Adição módulo 8 aulas 1-5

---
+++ modulo8_javascript_basico/aula4_variaveis/script.js
@@ -0,0 +1,8 @@
+var a;
+var b = "HDD";
+
+b = a;
+a = "Placa de captura";
+
+console.log("a = " + a);
+console.log("b = " + b);
---
+++ modulo8_javascript_basico/aula5_tipos_dados/script.js
@@ -0,0 +1,23 @@
+// String
+var str = "A;\nB;\nC.";
+console.log(str)
+console.log("1" + "1")
+
+// Number
+var number = 1;
+console.log(number)
+console.log(1 + 1)
+
+// Null
+var nll = null;
+console.log(nll)
+
+// Boolean
+var bool1 = false
+var bool2 = true
+console.log(bool1)
+console.log(bool2)
+
+// Undefined
+var und;
+console.log(und)

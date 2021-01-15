61797a9d129f94e9e59f41a3da8e6b5017cf6d37
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-14 23:29:17 -0300

    Adição módulo 8 aula 6

---
+++ modulo8_javascript_basico/aula6_operacoes_aritmeticas/script.js
@@ -0,0 +1,31 @@
+var a = 10;
+var b = 15;
+
+var c = a + b;
+console.log(c);
+
+var d = a - b;
+console.log(d);
+
+var e = a * b;
+console.log(e);
+
+var f = a / b;
+console.log(f);
+
+var g = a % b;
+console.log(g);
+
+// a = a + 1;
+a++;
+console.log(a);
+
+// b = b - 1;
+b--;
+console.log(b);
+
+var a = 30;
+// var b = --a;
+var b = a--;
+console.log(b);
+console.log(a);

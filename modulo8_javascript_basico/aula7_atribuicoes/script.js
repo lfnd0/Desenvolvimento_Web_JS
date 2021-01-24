0e2b4ee7047908624e655014da6471857d26b61c
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-23 23:15:42 -0300

    Adição módulo 8 aulas 7-8

---
+++ modulo8_javascript_basico/aula7_atribuicoes/script.js
@@ -0,0 +1,19 @@
+var a = 30;
+a += 2;
+console.log(a);
+
+var b = 30;
+b -= 2;
+console.log(b);
+
+var c = 30;
+c -= 2;
+console.log(c);
+
+var d = 30;
+d /= 2;
+console.log(d);
+
+var e = 30;
+e %= 28;
+console.log(e);
---
+++ modulo8_javascript_basico/aula8_operadores_comparativos/script.js
@@ -0,0 +1,25 @@
+var a = 2;
+var b = 3;
+console.log(a == b);
+
+var c = 3;
+var d = "3";
+console.log(c === d);
+
+var e = 3;
+var f = 3;
+console.log(e != f);
+
+var e = 3;
+var f = "3";
+console.log(e !== f);
+
+var g = 5;
+var h = 3;
+console.log(g > h);
+console.log(g < h);
+
+var i = 4;
+var j = 4;
+console.log(i >= j);
+console.log(i <= j);

f0a89b52770d96e81c3c911c69f427728081f99a
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-25 21:47:08 -0300

    Adição módulo 8 aulas 11-12

---
+++ modulo8_javascript_basico/aula11_estruturas_condicionais/script.js
@@ -0,0 +1,9 @@
+var age = 12;
+
+if (age >= 18 && age <= 27) {
+    console.log("Liberado!\nMostre sua identidade!");
+} else if (age >= 28 && age <= 67) {
+    console.log("Liberado!\nQual o seu pedido?");
+} else {
+    console.log("Bloqueado!");
+}
---
+++ modulo8_javascript_basico/aula12_operador_ternario/script.js
@@ -0,0 +1,7 @@
+var age = 16;
+
+// age >= 18 ? console.log("Liberado!") : console.log("Bloqueado!");
+// var ageChecker = age >= 18 ? true : false;
+
+var ageChecker = age >= 18 ? "Liberado!" : "Bloqueado!";
+console.log(ageChecker);

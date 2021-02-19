272c3ef9e58571cff06a71c8414a1e037b8f6391
luizfernando <luiz-fsilva37@hotmail.com>
2021-02-19 01:39:11 -0300

    Atualização módulo 8 aulas 11-21

---
+++ modulo8_javascript_basico/aula12_alert_prompt/script.js
@@ -0,0 +1,6 @@
+var myName = prompt("Digite o seu nome: ");
+alert("Ola " + myName + "!");
+
+var num = parseInt(prompt("Digite um numero: "));
+var double = num + num;
+alert("O dobro de " + num + " e: " + double);

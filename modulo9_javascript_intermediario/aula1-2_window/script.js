38ba78acf8f04606c762d025dd62384104b41d3b
luizfernando <luiz-fsilva37@hotmail.com>
2021-02-24 07:52:23 -0300

    Adição módulo 9 aulas 1-4

---
+++ modulo9_javascript_intermediario/aula1-2_window/script.js
@@ -0,0 +1,5 @@
+window.console.log("Hello window.console!");
+
+console.log("Hello not-window-console!");
+
+alert("Script carregado");
---
+++ modulo9_javascript_intermediario/aula3-4_document/script.js
@@ -0,0 +1,7 @@
+console.log(document);
+
+console.log(document.body);
+
+console.log(document.body.innerHTML);
+
+console.log(document.body.innerHTML = "<h1>Document</h1>" + document.body.innerHTML);

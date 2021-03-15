c369fae15460f9ac1deb86c822b3ba9c55708b29
luizfernando <luiz-fsilva37@hotmail.com>
2021-03-15 19:57:17 -0300

    Atualização módulo 9 aulas 7-8

---
+++ modulo9_javascript_intermediario/aula8_eventos_dom_pt2/script.js
@@ -0,0 +1,11 @@
+function pageLoad() {
+    console.log("Pagina carregada");
+}
+
+function onOver(element) {
+    element.style.backgroundColor = "red";
+}
+
+function onOut(element) {
+    element.style.backgroundColor = "cornflowerblue";
+}

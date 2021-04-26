e3945f9413902a271600f84182914b6d6feed1dc
luizfernando <luiz-fsilva37@hotmail.com>
2021-04-25 23:40:31 -0300

    Atualização módulo 9 aula 14

---
+++ modulo9_javascript_intermediario/aula14_biblioteca_math/script.js
@@ -0,0 +1,22 @@
+let mPI = Math.PI;
+console.log(mPI);
+
+let mE = Math.E;
+console.log(mE);
+
+let mSqtr = Math.sqrt(73);
+console.log(mSqtr);
+
+let mRandom = Math.round(Math.random() * 10);
+console.log(mRandom);
+
+let mRound = Math.round(9.8);
+console.log(mRound);
+
+// Piso
+let mFloor = Math.floor(7.6);
+console.log(mFloor);
+
+// Teto
+let mCeil = Math.ceil(5.1);
+console.log(mCeil);
